e401982a3f3050f34c1f86d9e776205a21ae489e
Matteo Trentin <matteo.trentin@studio.unibo.it>
2022-03-30 16:18:04 +0200

    feat: add initial bollard functions
    Add bollard dependencies for docker image and container creation; add a simple main function that loads an image, creates a container, prints its logs and deletes the created container.

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,57 @@
 // specific language governing permissions and limitations
 // under the License.
 //
-fn main() {
-    println!("Hello, world!");
+
+use bollard::image::CreateImageOptions;
+use futures_util::stream::StreamExt;
+
+use bollard::container::{CreateContainerOptions, Config, StartContainerOptions, RemoveContainerOptions, LogsOptions};
+use bollard::{Docker, API_DEFAULT_VERSION};
+
+async fn get_image(docker: &Docker, image_name: &str) -> Result<(), String> {
+    let image = &mut docker.create_image(Some(CreateImageOptions{
+        from_image: image_name,
+        ..Default::default()
+    }), None, None);
+
+    while let Some(Ok(l)) = image.next().await {
+        println!("{:?} {:?}", l.status.unwrap_or("".to_string()), l.progress.unwrap_or("".to_string()));
+    }
+
+    Ok(())
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let docker = Docker::connect_with_socket("/run/user/1001/docker.sock", 10, API_DEFAULT_VERSION)?;
+
+    get_image(&docker, "alpine:latest").await?;
+
+    let options = Some(CreateContainerOptions {
+        name: "funless-alpine-container"
+    });
+    let config = Config {
+        image: Some("alpine:latest"),
+        cmd: Some(vec!["echo", "hello"]),
+        ..Default::default()
+    };
+    
+    let container_response = docker.create_container(options, config).await?;
+    println!("Container ID: {:?}", container_response.id);
+    docker.start_container("funless-alpine-container", None::<StartContainerOptions<String>>).await?;
+
+    let log_options = Some(LogsOptions::<String> {
+        stdout: true,
+        ..Default::default()
+    });
+    let logs = &mut docker.logs("funless-alpine-container", log_options).take(1);
+
+    while let Some(Ok(l)) = logs.next().await {
+        println!("Logs: {:?}", l);
+    }
+
+    docker.remove_container("funless-alpine-container", None::<RemoveContainerOptions>).await?;
+    println!("Container removed.");
+
+    Ok(())
 }
