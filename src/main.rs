f0f2f24fef5981369eb1cfdc02072e1330680281
Matteo Trentin <matteo.trentin@studio.unibo.it>
2022-03-31 20:10:57 +0200

    feat: execute external code inside container
    Execute external code, packed in a tar archive, inside a previously initialized container; add a .tar.gz archive containing a simple js function for testing purposes; remove the unused container_spec.json file.

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,15 @@
 // under the License.
 //
 
+use std::fs::File;
+use std::io::Read;
+
 use bollard::image::CreateImageOptions;
 use futures_util::stream::StreamExt;
 
-use bollard::container::{CreateContainerOptions, Config, StartContainerOptions, RemoveContainerOptions, LogsOptions};
+use bollard::container::{CreateContainerOptions, Config, StartContainerOptions, RemoveContainerOptions, LogsOptions, LogOutput, UploadToContainerOptions, WaitContainerOptions};
 use bollard::{Docker, API_DEFAULT_VERSION};
+
 
 async fn get_image(docker: &Docker, image_name: &str) -> Result<(), String> {
     let image = &mut docker.create_image(Some(CreateImageOptions{
@@ -35,36 +39,73 @@ async fn get_image(docker: &Docker, image_name: &str) -> Result<(), String> {
     Ok(())
 }
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
-    let docker = Docker::connect_with_socket("/run/user/1001/docker.sock", 10, API_DEFAULT_VERSION)?;
+async fn setup_container(docker: &Docker, container_name: &str, image_name: &str, main_file: &str, tar_file: &str) -> Result<(), Box<dyn std::error::Error>> {
 
-    get_image(&docker, "alpine:latest").await?;
-
+    get_image(&docker, image_name).await?;
+    
     let options = Some(CreateContainerOptions {
-        name: "funless-alpine-container"
+        name: container_name
     });
+    let command = format!("require('{main_file}').main()");
+
     let config = Config {
-        image: Some("alpine:latest"),
-        cmd: Some(vec!["echo", "hello"]),
+        image: Some(image_name),
+        cmd: Some(vec!["node", "-e", command.as_str()]),
         ..Default::default()
     };
     
-    let container_response = docker.create_container(options, config).await?;
-    println!("Container ID: {:?}", container_response.id);
-    docker.start_container("funless-alpine-container", None::<StartContainerOptions<String>>).await?;
+    let _container_response = docker.create_container(options, config).await?;
+
+
+    let upload_options = Some(UploadToContainerOptions {
+        path: "/opt",
+        ..Default::default()
+    });
+    let mut file = File::open(tar_file)?;
+    let mut tar_body = Vec::new();
+    file.read_to_end(&mut tar_body)?;
+
+    docker.upload_to_container(container_name, upload_options, tar_body.into()).await?;
+
+    docker.start_container(container_name, None::<StartContainerOptions<String>>).await?;
+
+    Ok(())
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    
+    const DOCKER_PATH: &str = "/run/user/1001/docker.sock";
+    const IMAGE_NAME: &str = "node:lts-alpine";
+    const CONTAINER_NAME: &str = "funless-node-container";
+    const TAR_FILE: &str = "./js/hello.tar.gz";
+    const MAIN_FILE: &str = "/opt/index.js";
+
+    let docker = Docker::connect_with_socket(DOCKER_PATH, 10, API_DEFAULT_VERSION)?;
+
+    setup_container(&docker, CONTAINER_NAME, IMAGE_NAME, MAIN_FILE, TAR_FILE).await?;
 
     let log_options = Some(LogsOptions::<String> {
         stdout: true,
+        stderr: true,
         ..Default::default()
     });
-    let logs = &mut docker.logs("funless-alpine-container", log_options).take(1);
+    let logs = &mut docker.logs(CONTAINER_NAME, log_options);
+
+    let wait_options = Some(WaitContainerOptions{
+        condition: "not-running"
+    });
+    let _wait_response = docker.wait_container(CONTAINER_NAME, wait_options).into_future().await;
 
     while let Some(Ok(l)) = logs.next().await {
-        println!("Logs: {:?}", l);
+        match l {
+            LogOutput::StdOut {message: m} => println!("Logs: {:?}", m),
+            LogOutput::StdErr { message: m } => eprintln!("Error: {:?}", m),
+            _ => continue
+        }
     }
 
-    docker.remove_container("funless-alpine-container", None::<RemoveContainerOptions>).await?;
+    docker.remove_container(CONTAINER_NAME, None::<RemoveContainerOptions>).await?;
     println!("Container removed.");
 
     Ok(())
