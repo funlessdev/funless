12db6dbadbf8bfe76a8734ca1dfc6ff22b23b369
Matteo Trentin <matteo.trentin@studio.unibo.it>
2022-04-19 17:50:56 +0200

    feat: parameterize all NIFs; handle messages from NIFs' environment

--- lib/fn.ex
+++ lib/fn.ex
@@ -1,15 +1,15 @@
 defmodule Fn do
   use Rustler, otp_app: :worker, crate: :fn
 
-  def prepare_container() do
+  def prepare_container(_container_name, _image_name, _tar_path, _main_file) do
     :erlang.nif_error(:nif_not_loaded)
   end
 
-  def run_function() do
+  def run_function(_container_name) do
     :erlang.nif_error(:nif_not_loaded)
   end
 
-  def cleanup() do
+  def cleanup(_container_name) do
     :erlang.nif_error(:nif_not_loaded)
   end
 end
