41f5e841f64d9d7c17012f81ad3dc408b51a143f
Matteo Trentin <matteo.trentin@studio.unibo.it>
2022-04-26 22:06:29 +0200

    feat(worker.ex): add basic worker behaviour
    The worker() function now handles basic requests (prepare_container, run_function, cleanup) and uses a fn_to_containers mapping to allow function invocation by name (instead of container name). Every task is executed as a separate actor and can freely crash in case of errors.

--- lib/worker/worker.ex
+++ lib/worker/worker.ex
@@ -16,7 +16,7 @@ defmodule Worker.Worker do
     Fn.run_function(container_name)
     receive do
       {:ok, logs} ->
-        IO.puts("Logs from container:\n#{logs}\n")
+        IO.puts("Logs from container:\n#{logs}")
       {:error, err} ->
         IO.puts("Error while running function: #{err}")
         :error
@@ -42,21 +42,39 @@ defmodule Worker.Worker do
   end
 
   def worker(fn_to_containers) do
+    #TODO: get result from spawned subprocesses and forward it to sender
+    #TODO: handle container replicas
+    #TODO: handle automatic removal of containers (scale-to-zero) for inactive functions
+    Process.flag(:trap_exit, true)
+    IO.inspect(fn_to_containers)
     receive do
-      {:prepare, _function, _image_name, _tar_path, _main_file, _sender} ->
-        IO.puts("prepare")
-        worker(fn_to_containers)
+      {:prepare, function_name, image_name, tar_path, main_file, _sender} ->
+        container_name = function_name <> "-funless-container"
+        _subproc_pid = spawn_link(__MODULE__, :prepare_container, [container_name, image_name, tar_path, main_file])
+        worker(Map.put(fn_to_containers, function_name, container_name))
 
-      {:run, _function, _sender} ->
+      {:run, function_name, _sender} ->
         IO.puts("run")
+        container_name = fn_to_containers[function_name]
+        _subproc_pid = spawn_link(__MODULE__, :run_function, [container_name])
         worker(fn_to_containers)
 
-      {:cleanup, _function} ->
+      {:cleanup, function_name, _sender} ->
         IO.puts("cleanup")
+        container_name = fn_to_containers[function_name]
+        _subproc_pid = spawn_link(__MODULE__, :cleanup, [container_name])
+        worker(Map.delete(fn_to_containers, function_name))
+
+      {:EXIT, pid, reason} ->
+        #TODO: handle when a prepare_container function exits and remove the related {function,container} tuple from map
+        IO.puts("Received an EXIT message")
+        IO.inspect({:EXIT, pid, reason})
         worker(fn_to_containers)
 
-      _ ->
-        IO.puts("other")
+      other_messages ->
+        IO.inspect(other_messages)
+        worker(fn_to_containers)
+
     end
   end
 
@@ -73,7 +91,7 @@ defmodule Worker.Worker do
 
   def init(_args) do
     IO.puts("started")
-    pid = spawn_link(__MODULE__, :worker, [[]])
+    pid = spawn_link(__MODULE__, :worker, [%{}])
     Process.register(pid, :worker)
     {:ok, pid}
   end
