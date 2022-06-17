69f9d3678792c3372a34ef36ab2adb080fc7e4c5
giusdp <dpl.gsp@tutanota.com>
2022-06-17 13:59:30 +0200

    build(Docker): change dockerfile to use alpine and distillery
    Multi-stage dockerfile starting with elixir-alpine, in which rust is installed and everything compiled.
    Then the binaries are copied to an empty alpine container and deps needed installed.

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Core.MixProject do
   def project do
     [
       app: :core,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
