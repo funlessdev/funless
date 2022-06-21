98ba95c235250477a9b9e77f94c5a1374147cf22
giusdp <dpl.gsp@tutanota.com>
2022-06-21 20:15:36 +0200

    ci(Github-Action): rename container action into image and build with new dockerfile

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule FunlessWorker.MixProject do
   def project do
     [
       app: :worker,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
