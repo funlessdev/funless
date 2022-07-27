385780e1134c041b8e4607a8786cccfca201c50e
giusdp <dpl.gsp@tutanota.com>
2022-07-27 12:22:04 +0200

    fix(function-storage): fix table used to not override new insertions

--- test/function_storage_test.exs
+++ test/function_storage_test.exs
@@ -17,7 +17,7 @@
 #
 
 defmodule FunctionStorageTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case
   alias Worker.Adapters.FunctionStorage.ETS
   alias(Worker.Domain.RuntimeStruct)
   import Mox, only: [verify_on_exit!: 1]
@@ -29,7 +29,7 @@ defmodule FunctionStorageTest do
     assert result == []
   end
 
-  test "insert_function_runtime adds {function_name, runtime} couple to the storage" do
+  test "insert_runtime adds {function_name, runtime} couple to the storage" do
     runtime = %RuntimeStruct{
       host: "127.0.0.1",
       port: "8080",
@@ -39,9 +39,26 @@ defmodule FunctionStorageTest do
     ETS.insert_runtime("test", runtime)
 
     assert ETS.get_runtimes("test") == [runtime]
+
+    ETS.delete_runtime("test", runtime)
   end
 
-  test "delete_function_runtime removes a {function_name, runtime} couple from the storage" do
+  test "multiple insert_runtime with the same function name adds the runtime to the list" do
+    runtime1 = %RuntimeStruct{name: "test-runtime"}
+    runtime2 = %RuntimeStruct{name: "test-runtime-2"}
+
+    ETS.insert_runtime("test", runtime1)
+    ETS.insert_runtime("test", runtime2)
+
+    [rt, rt2] = ETS.get_runtimes("test")
+    assert rt.name == "test-runtime"
+    assert rt2.name == "test-runtime-2"
+
+    ETS.delete_runtime("test", rt)
+    ETS.delete_runtime("test", rt2)
+  end
+
+  test "delete_runtime removes a {function_name, runtime} couple from the storage" do
     runtime = %RuntimeStruct{
       host: "127.0.0.1",
       port: "8080",
@@ -53,5 +70,11 @@ defmodule FunctionStorageTest do
     ETS.delete_runtime("test-delete", runtime)
 
     assert ETS.get_runtimes("test-delete") == []
+  end
+
+  test "delete_runtime on empty storage works" do
+    rt = %RuntimeStruct{name: "name"}
+    result = ETS.delete_runtime("test", rt)
+    assert result == {:ok, {"test", rt}}
   end
 end
