025f22429e61a9dff784fbf3b42d1bbaf9fe7ce2
giusdp <dpl.gsp@tutanota.com>
2022-08-22 15:14:08 +0200

    feat(Docker): add env var to change docker socket path

--- init_container.sh
+++ init_container.sh
@@ -18,11 +18,15 @@
 #
 
 # The container should be run with -v /var/run/docker.sock:/var/run/docker-host.sock --network host
+# e.g. docker run --rm -v /var/run/docker.sock:/var/run/docker-host.sock --network host image-name
+
+# To specify another docker socket, use the -e DOCKER_HOST=<path>
+DOCKER_SOCK="${DOCKER_HOST:-/var/run/docker.sock}"
 
 echo "Launching worker in daemon mode"
 /home/funless/worker/bin/worker daemon
 
-echo "proxy socket is listening on /var/run/docker.sock"
-test -S /var/run/docker.sock || exec sudo /usr/bin/socat \
-  UNIX-LISTEN:/var/run/docker.sock,fork,mode=660,user=funless \
+echo "proxy socket is listening on ${DOCKER_SOCK}"
+test -S ${DOCKER_SOCK} || exec sudo /usr/bin/socat \
+  UNIX-LISTEN:${DOCKER_SOCK},fork,mode=660,user=funless \
   UNIX-CONNECT:/var/run/docker-host.sock
