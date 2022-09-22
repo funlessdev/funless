24c8753c912224897a73fbbe0befd1e0e98531f0
Matteo Trentin <matteo.trentin2@unibo.it>
2022-09-22 02:18:18 +0200

    feat(telemetry): add worker telemetry request server

---
+++ lib/worker/adapters/telemetry/request_server.ex
@@ -0,0 +1,48 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+defmodule Worker.Adapters.Telemetry.RequestServer do
+  @moduledoc """
+    Implements GenServer behaviour; the actor receives telemetry :pull requests, and returns the contents of the relative ETS table.
+  """
+  use GenServer, restart: :permanent
+  require Logger
+
+  def start_link(args) do
+    GenServer.start_link(__MODULE__, args, name: :worker_telemetry)
+  end
+
+  @impl true
+  def init(_args) do
+    # Process.flag(:trap_exit, true)
+    Logger.info("Telemetry Request Server: started")
+    {:ok, nil}
+  end
+
+  @impl true
+  def handle_call(:pull, _from, _state) do
+    res = :ets.lookup(:worker_telemetry, :resources) |> Enum.map(fn {_k, v} -> v end)
+
+    reply =
+      case res do
+        [r | _] -> {:ok, r}
+        [] -> {:error, :not_found}
+      end
+
+    {:reply, reply, nil}
+  end
+end
