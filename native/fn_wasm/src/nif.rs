b9d2d2ef5519c40e4fe77fb7ded6a0791a6490d8
Matteo Trentin <matteo.trentin2@unibo.it>
2022-09-30 19:11:53 +0200

    feat(wasm): add wasm run_function nif
    Add fn_wasm run_function nif, to invoke functions compiled to WebAssembly, using the wasmtime runtime.

---
+++ native/fn_wasm/src/nif.rs
@@ -0,0 +1,64 @@
+use crate::{atoms, utils};
+
+use rustler::{types::Binary, Encoder, Env, OwnedEnv};
+use std::thread;
+
+use wasi_common::pipe::ReadPipe;
+use wasmtime::*;
+
+#[rustler::nif]
+fn run_function(env: Env, code: Binary, args: String) {
+    let pid = env.pid();
+    if let Some(owned_code) = code.to_owned() {
+        thread::spawn(move || {
+            let mut thread_env = OwnedEnv::new();
+            let engine = Engine::default();
+
+            let (stdout, stdout_lock) = utils::make_write_pipe();
+            let (stderr, stderr_lock) = utils::make_write_pipe();
+            let stdin = ReadPipe::from(args);
+
+            match utils::get_main_func(&engine, owned_code.as_slice(), stdin, stdout, stderr) {
+                Ok((main_func, mut store)) => {
+                    let result = main_func.call(&mut store, ());
+
+                    let (lock, atom) = match result {
+                        Ok(_) => (stdout_lock, atoms::ok()),
+                        Err(_) => (stderr_lock, atoms::error()),
+                    };
+
+                    let extracted = utils::extract_string(lock);
+
+                    if let Some(output) = extracted {
+                        if let Ok(s) = output {
+                            thread_env.send_and_clear(&pid, |env| (atom, &s).encode(env))
+                        } else {
+                            thread_env.send_and_clear(&pid, |env| {
+                                (atoms::error(), atoms::extract_from_utf8_error()).encode(env)
+                            })
+                        }
+                    } else {
+                        thread_env.send_and_clear(&pid, |env| {
+                            (atoms::error(), atoms::lock_error()).encode(env)
+                        })
+                    }
+                }
+                Err(error) => thread_env.send_and_clear(&pid, |env| {
+                    (atoms::error(), (atoms::code_error(), error.to_string())).encode(env)
+                }),
+            }
+        });
+    } else {
+        env.send(
+            &pid,
+            (
+                atoms::error(),
+                (
+                    atoms::internal_error(),
+                    "Couldn't allocate code for separate thread",
+                ),
+            )
+                .encode(env),
+        )
+    }
+}
