34507076fa899c2aa22116bdb3c83a8ac76ab55c
giusdp <dpl.gsp@tutanota.com>
2022-10-01 11:10:17 +0200

    refactor: move core to apps as umbrella child

---
+++ apps/core/mix.exs
@@ -0,0 +1,64 @@
+defmodule Core.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :core,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.14",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
+      deps: deps()
+    ]
+  end
+
+  # Configuration for the OTP application.
+  #
+  # Type `mix help compile.app` for more information.
+  def application do
+    [
+      mod: {Core.Application, []},
+      extra_applications: [:logger, :mnesia, :runtime_tools],
+      start_phases: [
+        init_db: Mix.env()
+      ]
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  # Specifies your project dependencies.
+  #
+  # Type `mix help deps` for examples and options.
+  defp deps do
+    [
+      {:plug, "~> 1.13"},
+      {:bandit, "~> 0.5.0"},
+      {:jason, "~> 1.3"},
+      {:libcluster, "~> 3.3"},
+      {:logger_file_backend, "~> 0.0.13"},
+
+      # dev deps
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:mox, "~> 1.0", only: :test},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+    ]
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      setup: ["deps.get"]
+    ]
+  end
+end
