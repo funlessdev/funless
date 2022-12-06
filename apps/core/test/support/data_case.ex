6d96d4dfb458ec1b9708b3e602d66111101edd87
Giuseppe De Palma <dpl.gsp@tutanota.com>
2022-12-06 15:47:49 +0100

    test(core): change tag to module tag and add new controller case template

--- apps/core/test/support/data_case.ex
+++ apps/core/test/support/data_case.ex
@@ -31,7 +31,6 @@ defmodule Core.DataCase do
   use ExUnit.CaseTemplate
 
   alias Ecto.Adapters.SQL.Sandbox
-  @tag integration_test: true
 
   using do
     quote do
@@ -41,6 +40,8 @@ defmodule Core.DataCase do
       import Ecto.Changeset
       import Ecto.Query
       import Core.DataCase
+
+      @moduletag integration_test: true
     end
   end
 
