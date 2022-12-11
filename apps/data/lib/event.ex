f397bddbf3cc2d2661a44bf8d668e5d27348ebd8
Matteo Trentin <matteo.trentin2@unibo.it>
2022-12-11 15:18:37 +0100

    feat(data): add connected event data type

---
+++ apps/data/lib/event.ex
@@ -0,0 +1,29 @@
+# Copyright 2022 Giuseppe De Palma, Matteo Trentin
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+defmodule Data.ConnectedEvent do
+  @moduledoc """
+    ConnectedEvent struct representing an event connected to a function through an Event Connector.
+
+    ## Fields
+      - type: the type of the event; defines the type of connector that will be spawned
+      - params: parameters passed to the connector
+  """
+  @type t :: %__MODULE__{
+          type: String.t(),
+          params: Map.t()
+        }
+  @enforce_keys [:type, :params]
+  defstruct [:type, :params]
+end
