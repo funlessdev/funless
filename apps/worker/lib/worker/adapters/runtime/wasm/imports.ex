d03f1086f15f1cb42da8ed179be62caa3220556f
Giuseppe De Palma <dpl.gsp@tutanota.com>
2023-02-27 19:18:14 +0100

    docs(worker): add license header and moduledoc

--- apps/worker/lib/worker/adapters/runtime/wasm/imports.ex
+++ apps/worker/lib/worker/adapters/runtime/wasm/imports.ex
@@ -1,5 +1,24 @@
+# Copyright 2023 Giuseppe De Palma, Matteo Trentin
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 defmodule Worker.Adapters.Runtime.Wasm.Imports do
-  @moduledoc false
+  @moduledoc """
+  Module containing the WASM imports for FunLess functions.
+
+  The imports are functions that are called by the WASM code. They get the WASM context as first
+  argument, which contains the memory and the caller.
+  """
   require Logger
 
   def fl_imports(input_payload, agent) do
