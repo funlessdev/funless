68e3a1ca6f8b42de31859f449e04db219db3d91a
Matteo Trentin <matteo.trentin2@unibo.it>
2023-03-21 10:55:35 +0100

    chore: add missing license header

--- config/config.exs
+++ config/config.exs
@@ -29,7 +29,6 @@ config :core, Core.Domain.Ports.Telemetry.Metrics, adapter: Core.Adapters.Teleme
 config :core, Core.Domain.Ports.Connectors.Manager, adapter: Core.Adapters.Connectors.Manager
 config :core, Core.Domain.Ports.DataSinks.Manager, adapter: Core.Adapters.DataSinks.Manager
 config :core, Core.Domain.Ports.SubjectCache, adapter: Core.Adapters.Subjects.Cache
-config :core, Core.Domain.Ports.AdminCache, adapter: Core.Adapters.Admins.Cache
 
 config :core,
   ecto_repos: [Core.Repo, Core.SubjectsRepo]
