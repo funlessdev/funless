aa9adaf12ae84dc99178c8e8805e0f52a6377275
Giuseppe De Palma <dpl.gsp@tutanota.com>
2023-03-23 17:37:43 +0100

    build: refactor release build and merge dockerfiles

--- apps/core/mix.exs
+++ apps/core/mix.exs
@@ -18,7 +18,7 @@ defmodule Core.MixProject do
   def project do
     [
       app: :core,
-      version: "0.6.0",
+      version: "0.8.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- apps/worker/mix.exs
+++ apps/worker/mix.exs
@@ -18,7 +18,7 @@ defmodule Worker.MixProject do
   def project do
     [
       app: :worker,
-      version: "0.6.0",
+      version: "0.8.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
